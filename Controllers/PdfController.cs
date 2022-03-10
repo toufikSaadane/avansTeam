284e38b9b3b0ae7512ebeab431cdd6931850ae1b
Miranda <50808996+MiriPV@users.noreply.github.com>
2022-03-10 12:29:50 +0100

    pdf
    aan pdf gewerkt

--- Controllers/PdfController.cs
+++ Controllers/PdfController.cs
@@ -6,6 +6,8 @@ using avansTeam.Data;
 using Microsoft.AspNetCore.Mvc;
 using avansTeam.Models;
 using Microsoft.EntityFrameworkCore;
+using Aspose.Pdf;
+
 namespace avansTeam.Controllers
 {
     public class PdfController : Controller
@@ -19,14 +21,31 @@ namespace avansTeam.Controllers
         }
 
         // GET: /<controller>/
-        public async Task<IActionResult> Index(int id)
+        public async Task<IActionResult> Index()
         {
-            var performances = await (from performance in _context.Performances
-                                where performance.Id == id select performance).Include(p => p)
+            var movies = await (from movie in _context.Movies
+                                where movie.Performances.Any()
+                                select movie).Include(p =>
+                                    p.Performances.Where(a =>
+                                            a.startTime >= DateTime.Now && a.startTime <= DateTime.Today.AddDays(5)).OrderBy(o => o.startTime))
                                                 .ThenInclude(performance => performance.Hall)
                                                 .ToListAsync();
 
-            return View(performances);
+            // Initialize document object
+            Document document = new Document();
+
+            // Add page
+            Page page = document.Pages.Add();
+
+            // Add text to new page
+            page.Paragraphs.Add(new Aspose.Pdf.Text.TextFragment("Bioscoopkaartje"));
+            page.Paragraphs.Add(new Aspose.Pdf.Text.TextFragment("Spider man"));
+            page.Paragraphs.Add(new Aspose.Pdf.Text.TextFragment("Far from Home"));
+            page.Paragraphs.Add(new Aspose.Pdf.Text.TextFragment("€12"));
+            // Save PDF 
+            document.Save("document.pdf");
+
+            return View(movies);
         }
     }
 
